6a944c8e8101f2569734f2d9dda47584a184653e
TheReal23 <65376048+TheReal23@users.noreply.github.com>
2020-05-21 20:38:59 -0700

    AL DIS COED!

--- script.js
+++ script.js
@@ -233,7 +233,7 @@ GameManager.prototype.addOneTile = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = 2;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -320,7 +320,7 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
+          // The mighty 65536 tile
           if (merged.value === 65536) self.won = true;
           
           // Sending high score
